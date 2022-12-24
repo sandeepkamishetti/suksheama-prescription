bd630e6d86e1c7f3ef2588d6242fda25609671fd
saianis <ganish37@gmail.com>
2022-12-24 19:58:12 +0530

    Added tablet data and mapped the timings

---
+++ src/TabletData.js
@@ -0,0 +1,58 @@
+const TabletInfo = [
+  {
+    value: "TAB.BAKWELL AP",
+    label: "TAB.BAKWELL AP",
+    foodRelation: [2, 4],
+    timing: 2,
+  },
+  {
+    value: "TAB.THIOWELL",
+    label: "TAB.THIOWELL",
+    foodRelation: [2, 4],
+    timing: 2,
+  },
+  {
+    value: "TAB.GLENROL DT",
+    label: "TAB.GLENROL DT",
+    foodRelation: [2, 4],
+    timing: 2,
+  },
+  {
+    value: "TAB.KETOROL DT",
+    label: "TAB.KETOROL DT",
+    foodRelation: [2, 4],
+    timing: 2,
+  },
+  {
+    value: "TAB.SEPIT MR",
+    foodRelation: [2, 4],
+    label: "TAB.SEPIT MR",
+    timing: 2,
+  },
+  {
+    value: "TAB.SETOLAC MR",
+    foodRelation: [2, 4],
+    label: "TAB.SETOLAC MR",
+    timing: 2,
+  },
+  {
+    value: "TAB.FLAMINATE",
+    foodRelation: [2, 4],
+    label: "TAB.FLAMINATE",
+    timing: 2,
+  },
+  {
+    value: "TAB.PIROWELL",
+    foodRelation: [2, 4],
+    label: "TAB.PIROWELL",
+    timing: 2,
+  },
+  {
+    value: "TAB.FLAMINATE SP",
+    foodRelation: [2, 4],
+    label: "TAB.FLAMINATE SP",
+    timing: 2,
+  },
+];
+
+export default TabletInfo;
