acdbee6ed310b2cccf6492de7bacabe00eab6671
saianis <ganish37@gmail.com>
2023-01-02 19:18:49 +0530

    Merging updated changes to Sandeep-Branch (#2)
    * Update README.md
    
    Commit test
    
    * commit test
    
    * Added tablet data and mapped the timings
    
    * update tablet data
    
    * added back button in second page to switch between pages without loosing data
    
    * update tablet data
    
    * update handle medicine one logic
    
    * Integrate the tablets dropdowns
    
    * update medicine state variable

---
+++ src/TabletData.js
@@ -0,0 +1,179 @@
+const TabletInfo = [
+  {
+    value: "TAB.BAKWELL AP",
+    label: "BAKWELL AP",
+    foodRelation: [2, 4],
+  },
+  {
+    value: "TAB.THIOWELL",
+    label: "THIOWELL",
+    foodRelation: [2, 4],
+  },
+  {
+    value: "TAB.GLENROL DT",
+    label: "GLENROL DT",
+    foodRelation: [2, 4],
+  },
+  {
+    value: "TAB.KETOROL DT",
+    label: "KETOROL DT",
+    foodRelation: [2, 4],
+  },
+  {
+    value: "TAB.SEPIT MR",
+    label: "SEPIT MR",
+    foodRelation: [2, 4],
+  },
+  {
+    value: "TAB.SETOLAC MR",
+    label: "SETOLAC MR",
+    foodRelation: [2, 4],
+  },
+  {
+    value: "TAB.FLAMINATE",
+    label: "FLAMINATE",
+    foodRelation: [2, 4],
+  },
+  {
+    value: "TAB.PIROWELL",
+    label: "PIROWELL",
+    foodRelation: [2, 4],
+  },
+  {
+    value: "TAB.FLAMINATE SP",
+    label: "FLAMINATE SP",
+    foodRelation: [2, 4],
+  },
+  {
+    value: "CAP.RAAP DSR",
+    label: "RAAP DSR",
+    foodRelation: [1],
+  },
+  {
+    value: "CAP.RQD",
+    label: "RQD",
+    foodRelation: [1],
+  },
+  {
+    value: "TAB.BOROFLEX",
+    label: "BOROFLEX",
+    foodRelation: [4],
+  },
+  {
+    value: "TAB.QXOLE",
+    label: "QXOLE",
+    foodRelation: [1],
+  },
+  {
+    value: "TAB.ORCERIN C2",
+    label: "ORCERIN C2",
+    foodRelation: [4],
+  },
+  {
+    value: "JOINT PLUS SACHET",
+    label: "JOINT PLUS SACHET",
+    foodRelation: [4],
+  },
+  {
+    value: "TAB.JOINT PLUS",
+    label: "JOINT PLUS",
+    foodRelation: [2, 6],
+  },
+  {
+    value: "TAB.ALTRAJOINT C2",
+    label: "ALTRAJOINT C2",
+    foodRelation: [2, 6],
+  },
+  {
+    value: "CAP.ROSIFIT TRIO",
+    label: "ROSIFIT TRIO",
+    foodRelation: [2, 6],
+  },
+  {
+    value: "TAB.MYOTOP 150 MG",
+    label: "MYOTOP 150 MG",
+    foodRelation: [2, 6],
+  },
+  {
+    value: "TAB.NUSON 150 MG",
+    label: "NUSON 150 MG",
+    foodRelation: [2, 6],
+  },
+  {
+    value: "TAB.AXOVIT",
+    label: "AXOVIT",
+    foodRelation: [4],
+  },
+  {
+    value: "TAB.ZINCOBION PLUS",
+    label: "ZINCOBION PLUS",
+    foodRelation: [4],
+  },
+  {
+    value: "CAP.ENHANCE",
+    label: "ENHANCE",
+    foodRelation: [4],
+  },
+  {
+    value: "TAB.CALBONA K2",
+    label: "CALBONA K2",
+    foodRelation: [3],
+  },
+  {
+    value: "TAB.MEGACEE",
+    label: "MEGACEE",
+    foodRelation: [4],
+  },
+  {
+    value: "TAB.OSTEOBION K2",
+    label: "OSTEOBION K2",
+    foodRelation: [4],
+  },
+  {
+    value: "TAB.CLABONA XT",
+    label: "CLABONA XT",
+    foodRelation: [3],
+  },
+  {
+    value: "CAP.CISSMORE",
+    label: "CISSMORE",
+    foodRelation: [2, 6],
+  },
+  {
+    value: "TAB.CEFZUM",
+    label: "CEFZUM",
+    foodRelation: [2, 6],
+  },
+  {
+    value: "CAP.QURECLAV 625",
+    label: "QURECLAV 625",
+    foodRelation: [2, 6],
+  },
+  {
+    value: "TAB.FABUXA 80 MG",
+    label: "FABUXA 80 MG",
+    foodRelation: [4],
+  },
+  {
+    value: "TAB.FOLAX G",
+    label: "FOLAX G",
+    foodRelation: [6],
+  },
+  {
+    value: "CALBONA SHOT LIQUID",
+    label: "CALBONA SHOT LIQUID",
+    foodRelation: [2, 6],
+  },
+  {
+    value: "Tab.Myorelax 4 mg",
+    label: "Myorelax 4 mg",
+    foodRelation: [2, 6],
+  },
+  {
+    value: "Tab.Myorelax 8 mg",
+    label: "Myorelax 8 mg",
+    foodRelation: [2, 6],
+  },
+];
+
+export default TabletInfo;
